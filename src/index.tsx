c73625a5b29f25606ea3b06729f6e75578e65d42
Vlnts <valentsionovaviktoria@gmail.com>
2022-11-25 11:43:04 +0200

    Create CodeEditor Component

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import * as esbuild from 'esbuild-wasm';
 import { useEffect, useState, useRef } from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import { CodeEditor } from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -26,6 +27,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -36,21 +39,38 @@ const App = () => {
         global: 'window',
       },
     });
-    setCode(result.outputFiles[0].text);
-    try {
-      eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          try{
+             eval(event.data);
+          } catch (err) {
+            const root = document.querySelector('#root')
+            root.innerHTML = '<div style="color: red;"><h4>Runtime error</h4>' + err + '</div>';
+            console.log(err);
+          }
+        },false)
+      </script>
+    </body>
+    </html>
+  `;
 
   return (
     <div>
+      <CodeEditor initialValue="const a = 1;" onChange={(value) => setInput(value)} />
       <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
