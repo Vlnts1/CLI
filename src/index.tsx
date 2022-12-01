a93fe1bb38544554bd593b2a1ca65c32fa0b49e5
Vlnts <valentsionovaviktoria@gmail.com>
2022-12-01 20:52:42 +0200

    Add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
-import { CodeCell } from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
+// import { CodeCell } from './components/code-cell';
 import { TextEditor } from './components/text-editor';
+import React from 'react';
 
 const App = () => {
   return (
     <>
       <TextEditor />
-      <CodeCell />
+      {/* <CodeCell /> */}
     </>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
