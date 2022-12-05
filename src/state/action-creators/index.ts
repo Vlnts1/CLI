1f15d986d46b4a6a13e1e8e8b41dcabb3cc0a98d
Vlnts <valentsionovaviktoria@gmail.com>
2022-12-05 21:27:51 +0200

    Add BundlesReducer, action-creators, actions

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,5 +1,8 @@
+import { Dispatch } from 'redux';
+import { bundle } from '../../bundler';
 import { ActionType } from '../action-types';
 import {
+  Action,
   DeleteCellAction,
   Direction,
   InsertCellAfterAction,
@@ -42,5 +45,25 @@ export const insertCellAfter = (id: string | null, cellType: CellTypes): InsertC
       id,
       type: cellType,
     },
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId,
+      },
+    });
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: result,
+      },
+    });
   };
 };
--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -32,4 +32,28 @@ export type UpdateCellAction = {
   };
 };
 
-export type Action = MoveCellAction | DeleteCellAction | InsertCellAfterAction | UpdateCellAction;
+export type BundleStartAction = {
+  type: ActionType.BUNDLE_START;
+  payload: {
+    cellId: string;
+  };
+};
+
+export type BundleCompleteAction = {
+  type: ActionType.BUNDLE_COMPLETE;
+  payload: {
+    cellId: string;
+    bundle: {
+      code: string;
+      err: string;
+    };
+  };
+};
+
+export type Action =
+  | MoveCellAction
+  | DeleteCellAction
+  | InsertCellAfterAction
+  | UpdateCellAction
+  | BundleStartAction
+  | BundleCompleteAction;
